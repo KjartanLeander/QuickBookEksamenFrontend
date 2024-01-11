70a2b69589d22bdfe3fb0aef4e5bd87ff697b82b
Kjartan <Kjartan.leander@hotmail.com>
2024-01-11 19:03:27 +0100

    Oprettet side med hotel information for specifik hotel id

---
+++ js/hotelInfo.js
@@ -0,0 +1,34 @@
+import { fetchAnyUrl } from "/js/modulejson.js";
+
+const urlParams = new URLSearchParams(window.location.search);
+const hotelId = urlParams.get('id');
+const urlHotelInfo = `http://localhost:8080/hotels/${hotelId}`;
+const hotelInfoContainer = document.getElementById("hotel-info-container");
+
+// Fetch hotel details
+async function fetchHotelDetails() {
+    try {
+        const hotelData = await fetchAnyUrl(urlHotelInfo);
+        const hotelInfoHtml = createHotelInfoHtml(hotelData);
+        hotelInfoContainer.innerHTML = hotelInfoHtml;
+    } catch (error) {
+        console.error("Error fetching hotel details:", error);
+    }
+}
+
+// Create HTML for hotel details
+function createHotelInfoHtml(hotelData) {
+    return `
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Hotel name: ${hotelData.name}</h5>
+                <p class="card-text">Street: ${hotelData.street}</p>
+                <p class="card-text">City: ${hotelData.city}</p>
+                <p class="card-text">Zip: ${hotelData.zip}</p>
+                <p class="card-text">Country: ${hotelData.country}</p>
+            </div>
+        </div>
+    `;
+}
+
+fetchHotelDetails();
