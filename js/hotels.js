251431f9fa9cb2c96b89945e799b678539e9f77b
Kjartan <Kjartan.leander@hotmail.com>
2024-01-11 18:58:57 +0100

    Oprettet side hvor man kan se alle hoteller

---
+++ js/hotels.js
@@ -0,0 +1,47 @@
+import { fetchAnyUrl } from "/js/modulejson.js";
+
+const urlHotels = "http://localhost:8080/hotels";
+const hotelsContainer = document.getElementById("hotels-container");
+
+// Creates a hotel card
+function createHotelCard(hotel) {
+    const hotelTemplate = document.createElement("div");
+    hotelTemplate.classList.add("col-6", "col-sm-6", "col-md-4", "col-lg-3", "mb-4");
+    hotelTemplate.innerHTML = `
+        <div class="card">
+            <div class="hotel">
+                <div class="hotel-name p-2">
+                    <div class="col-12">
+                        <h5 class="card-title text-center">${hotel.name}</h5>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <a href="hotelInfo.html?id=${hotel.hotelId}">
+                            <div class="d-grid gap-2">
+                                <button class="btn btn-primary">See more info</button>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+    return hotelTemplate;
+}
+
+// Adds hotels from the backend to HTML
+async function fetchHotels() {
+    try {
+        const hotels = await fetchAnyUrl(urlHotels);
+
+        hotels.forEach(hotel => {
+            const hotelCard = createHotelCard(hotel);
+            hotelsContainer.appendChild(hotelCard);
+        });
+    } catch (error) {
+        console.error("Error fetching hotels:", error);
+    }
+}
+
+fetchHotels();
